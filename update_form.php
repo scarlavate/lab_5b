119c5f5df6bb8e4f106a951ad287fd8054aee933
Corrina Dahayu <rinkthcdc@gmail.com>
2024-12-22 19:47:50 +0700

    adding css

--- update_form.php
+++ update_form.php
@@ -30,6 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Update User</title>
+        <link rel="stylesheet" href="update_form.css">
     </head>
 
     <body>
